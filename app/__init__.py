eea25a6589007b43a77f579ec8af9b65e0edcbde
Lucas Nobre Barbosa <lucasbnobre18@gmail.com>
2023-10-02 12:09:07 -0300

    feat: add database implementation

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
-from flask import Flask
+import os
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 print(__name__)
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app.controllers import index, login, signup
+from app.models import tables
--- app/controllers/index.py
+++ app/controllers/index.py
@@ -6,4 +6,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
+    database_url = app.config.get("SQLALCHEMY_DATABASE_URI")
+    secret_key = app.config.get("SECRET_KEY")
+    print(f'database_url: {database_url}\nsecret_key: {secret_key}')
     return render_template('home.html')
