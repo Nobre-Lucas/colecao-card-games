fd5d96ad20f10863c9bfa39188b36256f68d3bcc
Lucas Nobre Barbosa <lucasbnobre18@gmail.com>
2023-10-04 09:37:06 -0300

    feat: add signup logic implementation

--- app/controllers/signup.py
+++ app/controllers/signup.py
@@ -1,14 +1,25 @@
 from flask import render_template
 
 from app import app
+from app import db
+
+from app.models.tables import User
 from app.models.forms import SignupForm
+
+import bcrypt
 
 @app.route('/sign-up', methods=['GET', 'POST'])
 def signup():
     signup_form = SignupForm()
-    print(signup_form)
     if signup_form.validate_on_submit():
-        print(signup_form.username.data)
+        username = signup_form.username.data
+        password = signup_form.password.data
+        name = signup_form.fullname.data
+        email = signup_form.email.data
+        new_user = User(username, password, name, email)
+        db.session.add(new_user)
+        db.session.commit()
+        return render_template('home.html')
     else:
         print(signup_form.errors)
     return render_template('signup.html', form=signup_form)
--- app/models/tables.py
+++ app/models/tables.py
@@ -1,22 +1,27 @@
 from app import db
 
+import bcrypt
 
 class User(db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
+    password_hashed = db.Column(db.String)
     name = db.Column(db.String)
     email = db.Column(db.String, unique=True)
     
     def __init__(self, username, password, name, email) -> None:
         super().__init__()
         self.username = username
-        self.password = password
+        self.password_hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         self.name = name
         self.email = email
         
     def __repr__(self) -> str:
         return f'<User {self.username}>'
+    
+    @property
+    def password(self):
+        raise AttributeError('password not readable')
     
